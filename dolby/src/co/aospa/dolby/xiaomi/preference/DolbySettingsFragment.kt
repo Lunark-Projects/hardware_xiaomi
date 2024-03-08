b350ca75acd25cb737779edeebd78c55df5a7372
Chaohui Wang <chaohuiw@google.com>
2024-03-08 15:24:40 +0530

    dolby: Migrate to CompoundButton.OnCheckedChangeListener
    Switch and SwitchCompat are both CompoundButton.
    
    Using CompoundButton in Java will helps migration in the future.
    
    Bug: 306658427 | AOSP | AOSP
    Test: manual - check Settings pages
    Test: m RunSettingsLibRoboTests
    Change-Id: I85a70d4c504d8584030ea4a058f30d74206ab835

--- dolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
+++ dolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
@@ -12,7 +12,7 @@ import android.media.AudioDeviceInfo
 import android.media.AudioManager
 import android.os.Bundle
 import android.os.Handler
-import android.widget.Switch
+import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.preference.ListPreference
 import androidx.preference.Preference
@@ -35,10 +35,9 @@ import co.aospa.dolby.xiaomi.DolbyConstants.Companion.dlog
 import co.aospa.dolby.xiaomi.DolbyController
 import co.aospa.dolby.xiaomi.R
 import com.android.settingslib.widget.MainSwitchPreference
-import com.android.settingslib.widget.OnMainSwitchChangeListener
 
 class DolbySettingsFragment : PreferenceFragment(),
-    OnPreferenceChangeListener, OnMainSwitchChangeListener {
+    OnPreferenceChangeListener, CompoundButton.OnCheckedChangeListener {
 
     private val switchBar by lazy {
         findPreference<MainSwitchPreference>(PREF_ENABLE)!!
@@ -200,8 +199,8 @@ class DolbySettingsFragment : PreferenceFragment(),
         return true
     }
 
-    override fun onSwitchChanged(switchView: Switch, isChecked: Boolean) {
-        dlog(TAG, "onSwitchChanged($isChecked)")
+    override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+        dlog(TAG, "onCheckedChanged($isChecked)")
         dolbyController.dsOn = isChecked
         profilePref.setEnabled(isChecked)
         updateProfileSpecificPrefs()
